9d31d724356bc3592b5f48c7d098eb4b206f49be
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-24 11:57:35 -0700

    Cache revgeo data

--- maps/Geo.js
+++ maps/Geo.js
@@ -1,28 +1,43 @@
 const NodeGeocoder = require('node-geocoder');
+const Nedb = require('nedb-promises');
 const Log = require('debug')('geo');
 
-const EMAIL = 'tim.j.wilkinson+kn6plv@gmail.com';
-const KEY = 'h-nQsZFRM-nEdMavIGavDBsYb8kZPiCK1-ku7I---AA';
+const PROVIDER = 'openstreetmap';
+//const PROVIDER = 'here';
+//const KEY = 'h-nQsZFRM-nEdMavIGavDBsYb8kZPiCK1-ku7I---AA';
+
+const DB_NAME = `${__dirname}/revgeo.db`;
 
 const geo = NodeGeocoder({
-  provider: 'here',
-  apiKey: KEY
+  provider: PROVIDER,
+  //apiKey: KEY
 });
-const cache = {};
+let db = null;
 
 class Geo {
+
+  constructor() {
+    if (db) {
+      return;
+    }
+    db = Nedb.create({ filename: DB_NAME, autoload: true });
+    db.persistence.setAutocompactionInterval(60 * 60 * 1000);
+  }
 
   async latlon2address(lat, lon) {
     try {
       const key = `${lat},${lon}`;
-      let r = cache[key];
-      if (!r) {
+      let r = await db.findOne({ _id: key });
+      Log(key, r);
+      if (r) {
+        r = JSON.parse(r.revgeo);
+      }
+      else {
         r = await geo.reverse({
           lat: lat,
-          lon: lon,
-          email: EMAIL
+          lon: lon
         });
-        cache[key] = r;
+        db.update({ _id: key }, { _id: key, revgeo: JSON.stringify(r) }, { upsert: true }).catch(e => Log(e));
       }
       let ans = null;
       if (r[0].streetNumber) {
